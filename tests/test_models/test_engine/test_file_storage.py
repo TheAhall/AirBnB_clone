1d8625a1e56b6f8700154a8f740f7ea78cd13fab
HALLAL <hmdhallal@gmail.com>
2024-03-09 09:06:01 -0800

    0x00. AirBnB clone - The console

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,52 +8,58 @@ from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
-        self.file_path = "test_file.json"
+        self.file_path = "file_test.json"
         self.storage = FileStorage()
         self.storage._FileStorage__file_path = self.file_path
         self.model = BaseModel()
+        self.model.name = "Test Model"
+        self.model.my_number = 123
+        self.model.save()
 
     def tearDown(self):
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
 
-    def test_all_method(self):
-        all_objs = self.storage.all()
-        self.assertIsInstance(all_objs, dict)
-        self.assertEqual(len(all_objs), 0)
+    def test_all(self):
+        all_objects = self.storage.all()
+        self.assertIsNotNone(all_objects)
+        self.assertIsInstance(all_objects, dict)
+        self.assertIn("BaseModel.{}".format(self.model.id), all_objects)
+        self.assertEqual(all_objects["BaseModel.{}".format(self.model.id)], self.model)
 
-    def test_new_method(self):
-        obj_id = self.model.id
-        self.storage.new(self.model)
-        all_objs = self.storage.all()
-        self.assertIn("BaseModel.{}".format(obj_id), all_objs.keys())
+    def test_new(self):
+        new_model = BaseModel()
+        new_model.name = "New Test Model"
+        new_model.my_number = 456
+        self.assertNotIn("BaseModel.{}".format(new_model.id), self.storage.all())
+        self.storage.new(new_model)
+        self.assertIn("BaseModel.{}".format(new_model.id), self.storage.all())
+        self.assertEqual(self.storage.all()["BaseModel.{}".format(new_model.id)], new_model)
 
-    def test_save_method(self):
-        obj_id = self.model.id
-        self.model.name = "Test Model"
-        self.storage.new(self.model)
+    def test_save_reload(self):
         self.storage.save()
-        
+        self.assertTrue(os.path.exists(self.file_path))
+
+        with open(self.file_path, 'r') as file:
+            data = json.load(file)
+
+        self.assertIn("BaseModel.{}".format(self.model.id), data)
+        self.assertEqual(data["BaseModel.{}".format(self.model.id)]['name'], self.model.name)
+        self.assertEqual(data["BaseModel.{}".format(self.model.id)]['my_number'], self.model.my_number)
+
+        # Now let's reload the storage
         new_storage = FileStorage()
+        new_storage._FileStorage__file_path = self.file_path
         new_storage.reload()
-        all_objs = new_storage.all()
-        
-        self.assertIn("BaseModel.{}".format(obj_id), all_objs.keys())
-        self.assertEqual(all_objs["BaseModel.{}".format(obj_id)]['name'], "Test Model")
+        reloaded_objects = new_storage.all()
 
-    def test_reload_method(self):
-        obj_id = self.model.id
-        self.model.name = "Test Model"
-        self.storage.new(self.model)
-        self.storage.save()
+        self.assertIn("BaseModel.{}".format(self.model.id), reloaded_objects)
+        reloaded_model = reloaded_objects["BaseModel.{}".format(self.model.id)]
+        self.assertIsInstance(reloaded_model, BaseModel)
+        self.assertEqual(reloaded_model.id, self.model.id)
+        self.assertEqual(reloaded_model.name, self.model.name)
+        self.assertEqual(reloaded_model.my_number, self.model.my_number)
 
-        new_storage = FileStorage()
-        new_storage.reload()
-        all_objs = new_storage.all()
-
-        self.assertIn("BaseModel.{}".format(obj_id), all_objs.keys())
-        self.assertEqual(all_objs["BaseModel.{}".format(obj_id)]['name'], "Test Model")
 
 if __name__ == '__main__':
     unittest.main()
-
